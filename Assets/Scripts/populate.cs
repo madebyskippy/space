1d4a34f9737483cf4614a8d6a1e671895b7f18a8
Unknown <laurenzriklin@gmx.at>
2017-09-25 22:18:28 -0400

    room outlines

--- Assets/Scripts/populate.cs
+++ Assets/Scripts/populate.cs
@@ -4,13 +4,100 @@ using UnityEngine;
 
 public class populate : MonoBehaviour {
 
-	// Use this for initialization
+	List<GameObject> rooms = new List <GameObject> ();
+
+	List<LineRenderer> roomOutlines = new List <LineRenderer> ();
+
+	[SerializeField] GameObject outlinePrefab;
+
+
+	void Awake () {
+
+		rooms.AddRange(GameObject.FindGameObjectsWithTag ("Room"));
+	}
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			Outline ();
+			Populate ();
+
+		}
+	}
+
+	void Populate() {
 		
 	}
+
+	void PopPeople() {
+		
+	}
+	// create line renderer outlines for rooms
+	void Outline () {
+		for (int i = 0; i < rooms.Count; i++) {
+			Mesh mesh = rooms[i].GetComponent<MeshFilter> ().mesh;
+			List<Vector3> cornerPoints = new List <Vector3> ();
+			mesh.GetVertices (cornerPoints);
+			for (int j = 0; j < cornerPoints.Count; j++) {
+				cornerPoints [j] = rooms[i].transform.TransformPoint (cornerPoints [j]);
+			}
+
+			//generate floor
+			GameObject floorOutlineGO = Instantiate(outlinePrefab, rooms[i].transform.position, Quaternion.identity);
+			LineRenderer floorLine = floorOutlineGO.GetComponent<LineRenderer> ();
+			roomOutlines.Add (floorLine);
+			floorLine.positionCount = 5;
+			floorLine.SetPosition (0,cornerPoints[1]);
+			floorLine.SetPosition (1,cornerPoints[7]);
+			floorLine.SetPosition (2,cornerPoints[6]);
+			floorLine.SetPosition (3,cornerPoints[0]);
+			floorLine.SetPosition (4,cornerPoints[1]);
+
+			// generate ceiling
+			GameObject ceilingOutlineGO = Instantiate(outlinePrefab, rooms[i].transform.position, Quaternion.identity);
+			LineRenderer ceilingLine = ceilingOutlineGO.GetComponent<LineRenderer> ();
+			roomOutlines.Add (ceilingLine);
+			ceilingLine.positionCount = 5;
+			ceilingLine.SetPosition (0,cornerPoints[3]);
+			ceilingLine.SetPosition (1,cornerPoints[5]);
+			ceilingLine.SetPosition (2,cornerPoints[4]);
+			ceilingLine.SetPosition (3,cornerPoints[2]);
+			ceilingLine.SetPosition (4,cornerPoints[3]);
+
+			// generate walls
+			GameObject wallOutline1 = Instantiate(outlinePrefab, rooms[i].transform.position, Quaternion.identity);
+			LineRenderer wallLine1 = wallOutline1.GetComponent<LineRenderer> ();
+			roomOutlines.Add (wallLine1);
+			wallLine1.positionCount = 2;
+			wallLine1.SetPosition (0,cornerPoints[7]);
+			wallLine1.SetPosition (1,cornerPoints[5]);
+			GameObject wallOutline2 = Instantiate(outlinePrefab, rooms[i].transform.position, Quaternion.identity);
+			LineRenderer wallLine2 = wallOutline2.GetComponent<LineRenderer> ();
+			roomOutlines.Add (wallLine2);
+			wallLine2.positionCount = 2;
+			wallLine2.SetPosition (0,cornerPoints[6]);
+			wallLine2.SetPosition (1,cornerPoints[4]);
+			GameObject wallOutline3 = Instantiate(outlinePrefab, rooms[i].transform.position, Quaternion.identity);
+			LineRenderer wallLine3 = wallOutline3.GetComponent<LineRenderer> ();
+			roomOutlines.Add (wallLine3);
+			wallLine3.positionCount = 2;
+			wallLine3.SetPosition (0,cornerPoints[0]);
+			wallLine3.SetPosition (1,cornerPoints[2]);
+			GameObject wallOutline4 = Instantiate(outlinePrefab, rooms[i].transform.position, Quaternion.identity);
+			LineRenderer wallLine4 = wallOutline4.GetComponent<LineRenderer> ();
+			roomOutlines.Add (wallLine4);
+			wallLine4.positionCount = 2;
+			wallLine4.SetPosition (0,cornerPoints[1]);
+			wallLine4.SetPosition (1,cornerPoints[3]);
+
+
+
+
+		}
+	}
 }
+	
