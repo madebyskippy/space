a70e9183c88c9580953249cb99d030ecbeaa6117
Unknown <laurenzriklin@gmx.at>
2017-10-16 18:35:03 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/connections.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class connections : MonoBehaviour {
+
+	[SerializeField] fitter fitScript;
+
+
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			
+		}
+	}
+
+	void GenerateConnections() {}
+}
