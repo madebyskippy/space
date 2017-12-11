32540867d442b550cb76b9d70e4546dc5e0f2112
madebyskippy <timothys@nyu.edu>
2017-12-11 18:59:00 -0500

    UI disappears now

--- Assets/Scripts/mouseScript.cs
+++ Assets/Scripts/mouseScript.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class mouseScript : MonoBehaviour {
 
+	[SerializeField] GameObject allCanvas;
+
+	Vector3 oldMousePos;
+	float mouseTimer;
+	float mouseStillTime = 4f; //seconds for mouse to be still until stuff fades
+
 	// Use this for initialization
 	void Start () {
-		
+		mouseTimer = 0f;
 	}
 	
 	// Update is called once per frame
@@ -17,6 +23,19 @@ public class mouseScript : MonoBehaviour {
 			if (objectHit.tag == "person") {
 				objectHit.GetComponent<person> ().hover();
 			}
+		}
+
+		Vector3 mousePos = Input.mousePosition;
+		if (mousePos == oldMousePos) {
+			mouseTimer += Time.deltaTime;
+		} else {
+			mouseTimer = 0;
+			allCanvas.SetActive (true);
+		}
+		oldMousePos = mousePos;
+
+		if (mouseTimer > mouseStillTime) {
+			allCanvas.SetActive (false);
 		}
 	}
 }
